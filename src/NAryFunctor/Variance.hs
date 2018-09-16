4a473b404236b1f78463cb0cd30c6c2d2df59441
Samuel Gélineau <gelisam+github@gmail.com>
2018-09-16 11:43:37 -0400

    simplify API using type families
    Now that we have simplified the problem by not attempting to support
    variance subtyping, we can also simplify the API, because each type
    constructor has precisely one variance for each type parameter.

--- src/NAryFunctor/Variance.hs
+++ src/NAryFunctor/Variance.hs
@@ -6,29 +6,47 @@ import Data.Bifunctor
 import Data.Functor.Const
 
 
-class VFunctor (v :: k -> k -> *) (f :: k) where
-  vmap :: v f f
+class VFunctor (f :: k) where
+  type VMap f :: k -> k -> *
+  vmap :: VMap f f f
 
 
--- Internals
+newtype CovariantMap1 cc f f' = CovariantMap1
+  { (<#>) :: forall a a'
+           . (a -> a')
+          -> cc (f a) (f' a')
+  }
 
-data Nonvariant1 a cc f f' = Nonvariant1
-  { (-#-) :: () -> cc (f a) (f' a) }
+newtype ContravariantMap1 cc f f' = ContravariantMap1
+  { (>#<) :: forall a a'
+           . (a' -> a)
+          -> cc (f a) (f' a')
+  }
 
-data Invariant1 cc f f' = Invariant1
-  { (<#>/>#<) :: forall a a'. (a -> a', a' -> a) -> cc (f a) (f' a') }
+newtype InvariantMap1 cc f f' = InvariantMap1
+  { (<#>/>#<) :: forall a a'
+               . (a -> a', a' -> a)
+              -> cc (f a) (f' a')
+  }
 
-data Covariant1 cc f f' = Covariant1
-  { (<#>) :: forall a a'. (a -> a') -> cc (f a) (f' a') }
-
-data Contravariant1 cc f f' = Contravariant1
-  { (>#<) :: forall a a'. (a' -> a) -> cc (f a) (f' a') }
-
-data Phantomvariant1 cc f f' = Phantomvariant1
-  { (👻#👻) :: forall a a'. () -> cc (f a) (f' a') }
+newtype PhantomvariantMap1 cc f f' = PhantomvariantMap1
+  { (👻#👻) :: forall a a'
+             . ()
+            -> cc (f a) (f' a')
+  }
 
 
 -- Instances
+
+-- |
+-- >>> vmap <#> length <#> length $ ("abc", "abcde")
+-- (3,5)
+instance VFunctor (,) where
+  type VMap (,) = CovariantMap1 (CovariantMap1 (->))
+  vmap = CovariantMap1 $ \f1
+      -> CovariantMap1 $ \f2
+      -> \(x1,x2)
+      -> (f1 x1, f2 x2)
 
 -- |
 -- >>> :{
@@ -37,35 +55,9 @@ data Phantomvariant1 cc f f' = Phantomvariant1
 -- in length' 100
 -- :}
 -- "3"
-instance v ~ Contravariant1 (Covariant1 (->)) => VFunctor v (->) where
-  vmap = Contravariant1 $ \f1'
-      -> Covariant1 $ \f2
+instance VFunctor (->) where
+  type VMap (->) = ContravariantMap1 (CovariantMap1 (->))
+  vmap = ContravariantMap1 $ \f1'
+      -> CovariantMap1 $ \f2
       -> \g
       -> f2 . g . f1'
-
--- |
--- >>> vmap <#> length 👻#👻 () $ Const "foo"
--- Const 3
-instance v ~ Covariant1 (Phantomvariant1 (->)) => VFunctor v Const where
-  vmap = Covariant1 $ \f1
-      -> Phantomvariant1 $ \()
-      -> \(Const x1)
-      -> Const (f1 x1)
-
--- |
--- >>> :{
--- let action :: State Int ()
---     action = modify (+1)
---     action' :: State String ()
---     action' = vmap <#>/>#< (show,read) -#- () <#> id $ action
--- in execState action' "42"
--- :}
--- "43"
-instance ( v ~ Invariant1 (Nonvariant1 m (Covariant1 (->)))
-         , Functor m
-         ) => VFunctor v StateT where
-  vmap = Invariant1 $ \(f1,f1')
-      -> Nonvariant1 $ \()
-      -> Covariant1 $ \f3
-      -> \(StateT body)
-      -> StateT (fmap (bimap f3 f1) . body . f1')
