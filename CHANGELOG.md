b3e61a191f4711b796c76d9a557056aea6000ecc
Samuel Gélineau <gelisam+github@gmail.com>
2018-09-16 17:46:15 -0400

    support higher-kinded type parameters

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 * A more general `VFunctor` typeclass supporting type parameters which vary
   covariantly, contravariantly, invariantly, or phantomvariantly.
+* Support for mapping over higher-kinded type parameters using covariant,
+  contravariant, or invariant natural transformations.
 
 ## New in 0.1.0.0
 
--- src/NAryFunctor/Variance.hs
+++ src/NAryFunctor/Variance.hs
@@ -1,9 +1,15 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, PolyKinds, RankNTypes, TypeFamilies, UndecidableInstances, UnicodeSyntax #-}
 module NAryFunctor.Variance where
 
+import Control.Arrow
 import Control.Monad.Trans.State
 import Data.Bifunctor
 import Data.Functor.Const
+
+-- $setup
+-- >>> import Control.Monad
+-- >>> import Control.Monad.IO.Class
+-- >>> import Data.Functor.Identity
 
 
 class VFunctor (f :: k) where
@@ -36,6 +42,30 @@ newtype PhantomvariantMap1 cc f f' = PhantomvariantMap1
   }
 
 
+
+newtype NF m m' = NF
+  { runNF :: forall a. m a -> m' a
+  }
+
+newtype Covariant1Map1 cc f f' = Covariant1Map1
+  { (<##>) :: forall m m'. (Functor m, Functor m')
+           => NF m m'
+           -> cc (f m) (f' m')
+  }
+
+newtype Contravariant1Map1 cc f f' = Contravariant1Map1
+  { (>##<) :: forall m m'. (Functor m, Functor m')
+           => NF m' m
+           -> cc (f m) (f' m')
+  }
+
+newtype Invariant1Map1 cc f f' = Invariant1Map1
+  { (<##>/>##<) :: forall m m'. (Functor m, Functor m')
+                => (NF m m', NF m' m)
+                -> cc (f m) (f' m')
+  }
+
+
 -- Instances
 
 -- |
@@ -61,3 +91,27 @@ instance VFunctor (->) where
       -> CovariantMap1 $ \f2
       -> \g
       -> f2 . g . f1'
+
+-- |
+-- >>> :{
+-- let divideState :: Double -> StateT Double Identity ()
+--     divideState x = do
+--       modify (/ x)
+--     divideState' :: Int -> StateT Int Maybe ()
+--     divideState' x = do
+--       guard (x /= 0)
+--       vmap <#>/>#< (round, fromIntegral)
+--               <##> NF (Just . runIdentity)
+--                <#> id
+--                  $ divideState (fromIntegral x)
+-- in execStateT (divideState' 2) 6
+-- :}
+-- Just 3
+instance VFunctor StateT where
+  type VMap StateT = InvariantMap1 (Covariant1Map1 (CovariantMap1 (->)))
+  vmap = InvariantMap1 $ \(f1, f1')
+      -> Covariant1Map1 $ \f2
+      -> CovariantMap1 $ \f3
+      -> \body
+      -> StateT $ \s'
+      -> fmap (f3 *** f1) $ runNF f2 $ runStateT body $ f1' s'
