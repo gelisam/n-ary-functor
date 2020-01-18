585dadc685f7050dec4f89c0d460f9b65ba1822f
Samuel Gélineau <gelisam+github@gmail.com>
2020-01-18 09:57:41 -0500

    switch from doctest-discover to cabal-doctest

--- test/doctests.hs
+++ test/doctests.hs
@@ -1,1 +1,10 @@
-{-# OPTIONS_GHC -F -pgmF doctest-discover #-}
+module Main where
+
+import Build_doctests (flags, pkgs, module_sources)
+import Test.DocTest (doctest)
+
+main :: IO ()
+main = do
+    doctest args
+  where
+    args = flags ++ pkgs ++ module_sources
